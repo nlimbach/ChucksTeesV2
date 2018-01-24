e9d2c1a19651f6aa7dfb729ecf098f1bff62aabd
Charlie Arrendale <carrendale@gmail.com>
2018-01-24 14:15:02 -0500

    wip

--- client/src/components/components/Shared/Nav/Navpills.js
+++ client/src/components/components/Shared/Nav/Navpills.js
@@ -25,19 +25,19 @@ export default withAuth(class Navpills extends React.Component {
         if (this.state.authenticated === null) return null;
         const authNav = this.state.authenticated ?
             <ul className="auth-nav">
-                <li><a href="javascript:void(0)" onClick={this.props.auth.logout}>Logout</a></li>
-                <li><Link to="/logout">Logout</Link></li>
+                <li className="border"><a href="javascript:void(0)" onClick={this.props.auth.logout}>Logout</a></li>
+                <li className="border"><Link to="/logout">Logout</Link></li>
             </ul> :
             <ul className="auth-nav">
-                <li><a href="javascript:void(0)" onClick={this.props.auth.login}>Login</a></li>
-                <li><Link to="/signup">Signup</Link></li>
+                <li className="border"><a href="javascript:void(0)" onClick={this.props.auth.login}>Login</a></li>
+                <li className="border"><Link to="/signup">Signup</Link></li>
             </ul>;
         return (
             <nav>
                 <ul>
                     <li className="btn red brandLogo"><Link to="/">Chucks Tees</Link></li>
-                    <li><Link to="/profile">Profile</Link></li>
-                    <li><Link to="/placeorder">Shop</Link></li>
+                    <li className="border"><Link to="/profile">Profile</Link></li>
+                    <li className="border"><Link to="/placeorder">Shop</Link></li>
                     {authNav}
                 </ul>
             </nav>
